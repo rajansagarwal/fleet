364ded7b4ac2a51bfdb8c3917b8a49568e41ac13
Rajan Agarwal <64426829+rajanwastaken@users.noreply.github.com>
2022-12-01 18:42:17 -0500

    Update egt.cpp

--- egt.cpp
+++ egt.cpp
@@ -22,3 +22,44 @@ for (int i = 0; i < 100; i++) {
   // Replace the current population with the next generation
   population = nextGeneration;
 }
+
+/*
+Generation 1:
+Organism 1: fitness = 0.8
+Organism 2: fitness = 0.7
+Organism 3: fitness = 0.9
+Organism 4: fitness = 0.6
+Organism 5: fitness = 0.5
+Organism 6: fitness = 0.4
+Organism 7: fitness = 0.3
+Organism 8: fitness = 0.2
+Organism 9: fitness = 0.1
+Organism 10: fitness = 0.0
+
+Generation 2:
+Organism 1: fitness = 0.8
+Organism 2: fitness = 0.7
+Organism 3: fitness = 0.9
+Organism 4: fitness = 0.7
+Organism 5: fitness = 0.6
+Organism 6: fitness = 0.4
+Organism 7: fitness = 0.3
+Organism 8: fitness = 0.2
+Organism 9: fitness = 0.1
+Organism 10: fitness = 0.0
+
+...
+
+Generation 100:
+Organism 1: fitness = 0.9
+Organism 2: fitness = 0.8
+Organism 3: fitness = 0.9
+Organism 4: fitness = 0.8
+Organism 5: fitness = 0.7
+Organism 6: fitness = 0.6
+Organism 7: fitness = 0.5
+Organism 8: fitness = 0.4
+Organism 9: fitness = 0.3
+Organism 10: fitness = 0.2
+
+*/
