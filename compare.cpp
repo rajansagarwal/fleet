6899d1c509f58ff5a542bfa0f0801deb7fc9e3b2
Rajan Agarwal <64426829+rajanwastaken@users.noreply.github.com>
2022-12-01 19:16:03 -0500

    Update compare.cpp

--- compare.cpp
+++ compare.cpp
@@ -18,3 +18,34 @@ for (int i = 0; i < generationCount; i++) {
   // Print the difference to the console
   Serial.println("Generation " + i + ": fitness difference = " + fitnessDifference);
 }
+
+void compareResults(String strategies, double actual_heart, double actual_lungs, double actual_kidney) {
+  // Compute the average payoff for each organ in the simulation
+  double simulated_heart = 0;
+  double simulated_lungs = 0;
+  double simulated_kidney = 0;
+  for (int i = 0; i < strategies.length(); i++) {
+    if (strategies[i] == STRATEGY_COOPERATE) {
+      simulated_heart += 3;
+      simulated_lungs += 3;
+      simulated_kidney += 3;
+    } else if (strategies[i] == STRATEGY_DEFECT) {
+      simulated_heart += 4;
+      simulated_lungs += 2;
+      simulated_kidney += 2;
+    }
+  }
+  simulated_heart /= strategies.length();
+  simulated_lungs /= strategies.length();
+  simulated_kidney /= strategies.length();
+
+  // Compute the mean squared error for each organ
+  double mse_heart = pow(simulated_heart - actual_heart, 2);
+  double mse_lungs = pow(simulated_lungs - actual_lungs, 2);
+  double mse_kidney = pow(simulated_kidney - actual_kidney, 2);
+
+  // Print the results
+  Serial.print(mse_heart);
+  Serial.print(mse_lungs);
+  Serial.print(mse_kideny);
+}
